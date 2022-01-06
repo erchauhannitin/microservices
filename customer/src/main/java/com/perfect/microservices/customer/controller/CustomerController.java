ee2d81ec042f2943b271a73ce59b113d618c485f
Nitin Chauhan <nitin.c.chauhan@ericsson.com>
2022-01-07 02:12:04 +0530

    Cloud config implemented

--- customer/src/main/java/com/perfect/microservices/customer/controller/CustomerController.java
+++ customer/src/main/java/com/perfect/microservices/customer/controller/CustomerController.java
@@ -4,21 +4,28 @@ import com.perfect.microservices.customer.input.CustomerRegistrationRequest;
 import com.perfect.microservices.customer.service.CustomerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("api/customer")
+@RefreshScope
 public class CustomerController {
 
     @Autowired CustomerService customerService;
+    @Value("${message: shalom}") private String message;
 
     @PostMapping("register")
     public void registerCustomer(@RequestBody CustomerRegistrationRequest registrationRequest){
         log.info("New Customer registration", registrationRequest);
         customerService.registerCustomer(registrationRequest);
+    }
+
+    @GetMapping("/message")
+    public String message(){
+        log.info("Message is ----- ", message);
+        return message;
     }
 }
