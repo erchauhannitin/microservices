c2867de7fc9ceee144f85cd607b2b7257f8b08a5
Nitin Chauhan <nitin.c.chauhan@ericsson.com>
2022-01-28 19:09:55 +0530

    Pretty Xml

--- customer/src/main/java/com/perfect/microservices/customer/config/PrettyJson.java
+++ customer/src/main/java/com/perfect/microservices/customer/config/PrettyJson.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
@@ -16,6 +17,10 @@ public class PrettyJson extends WebMvcConfigurationSupport {
             if (converter instanceof MappingJackson2HttpMessageConverter) {
                 MappingJackson2HttpMessageConverter jsonConverter = (MappingJackson2HttpMessageConverter) converter;
                 jsonConverter.setPrettyPrint(true);
+            }
+            if (converter instanceof MappingJackson2XmlHttpMessageConverter) {
+                MappingJackson2XmlHttpMessageConverter xmlConverter = (MappingJackson2XmlHttpMessageConverter) converter;
+                xmlConverter.setPrettyPrint(true);
             }
         });
     }
